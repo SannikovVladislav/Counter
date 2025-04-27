0547b67f6c6801bde5ba07419c0b21043348ac44
Vladislav <sannikovladislav@mail.ru>
2025-04-27 22:04:30 +0300

    Создан проект

---
+++ Counter/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Владислав on 27.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var counter: UILabel!
+    
+    @IBOutlet weak var countButton: UIButton!
+    
+    var number: Int = 0
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        counter.text = "Значение счетчика: \(number)"
+        countButton.titleLabel?.text = "Считай!"
+    }
+    @IBAction func actionButton(_ sender: Any) {
+        counter.text = "Значение счетчика: \(number + 1)"
+        return number += 1
+    }
+}
+
+
